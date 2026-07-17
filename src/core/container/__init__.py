ec69d92bf82486a4bdd6d43fe915737059abfd67
Kilo <kilo@local>
2026-07-17 15:43:44 +0000

    feat(container): add InfraContainer + AppContainer2 (unused)

---
+++ src/core/container/__init__.py
@@ -0,0 +1,4 @@
+from src.core.container.app import AppContainer2 as AppContainer
+from src.core.container.infra import InfraContainer
+
+__all__ = ["AppContainer", "AppContainer2", "InfraContainer"]
